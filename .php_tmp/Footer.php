085dd51708176164dd20535d5167bbb18551ef39
RVPutian <rvputian2004@gmail.com>
2025-05-26 08:22:54 +0800

    Remove unused temporary files and update header links for correct paths

--- .php_tmp/Footer.php
+++
@@ -1,4 +0,0 @@
-<footer style="background: #b60606; color: #fff; text-align: center; padding: 18px 0; margin-top: auto;">
-   
-    &copy; 2025 Pokémon Project | PROJECT TASK
-</footer>
--- components/Header.php
+++ components/Header.php
@@ -1,10 +1,10 @@
 
 <header class="main-header">
     <nav class="main-nav">
-        <img src="/assets/img/logo-invi.png" alt="logo" class = "logo-head">
-        <button class="main-btn" onclick="window.location.href='/index.php'">Home</button>
-        <button class="main-btn" onclick="window.location.href='/page/Card.php'">Trainer Card</button>
-        <button class="main-btn" onclick="window.location.href='/page/PC.php'">PC</button>
+        <img src="/AD-Task-1/assets/img/logo-invi.png" alt="logo" class="logo-head">
+        <button class="main-btn" onclick="window.location.href='/AD-Task-1/index.php'">Home</button>
+        <button class="main-btn" onclick="window.location.href='/AD-Task-1/page/Card.php'">Trainer Card</button>
+        <button class="main-btn" onclick="window.location.href='/AD-Task-1/page/PC.php'">PC</button>
         <button class="main-btn" onclick="window.location.href='#'">Temp</button>
 
 
